d5abe0c32823aa5486f720054f72f733836d4c8e
Eiqko <sarandaeiko@gmail.com>
2019-12-27 19:40:19 +0700

    Updates

---
+++ precache-manifest.cce1899f967e96702d20aa8dc0ee3f39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7db354e022cd2a1e421eeb81ed094f1",
+    "url": "/todolist-demo/index.html"
+  },
+  {
+    "revision": "15dc85bff93c80b18da2",
+    "url": "/todolist-demo/static/css/main.1d956426.chunk.css"
+  },
+  {
+    "revision": "7704d6fe3cb74a5d1b4f",
+    "url": "/todolist-demo/static/js/2.8090fbc2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todolist-demo/static/js/2.8090fbc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15dc85bff93c80b18da2",
+    "url": "/todolist-demo/static/js/main.5f3bed13.chunk.js"
+  },
+  {
+    "revision": "bf7d62ea3bdc608295fd",
+    "url": "/todolist-demo/static/js/runtime-main.5535a8b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todolist-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
